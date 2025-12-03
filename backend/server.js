62c8c331aa176310c0795fe16037083370c6671a
MR-Titus02 <titusroxsan@gmail.com>
2025-12-03 21:51:39 +0530

    added message route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './src/routes/auth.route.js';
+import messageRoutes from './src/routes/message.route.js';
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
 
 
 app.listen(PORT, () => {
