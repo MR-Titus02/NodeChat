4ceecc3e07e9633f71a702a72fb2288818bf50fa
MR-Titus02 <titusroxsan@gmail.com>
2025-12-03 22:59:52 +0530

    app running as one (front & back)

---
+++ backend/src/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+
+dotenv.config();
+
+const app = express();
+const __dirname = path.resolve();
+
+const PORT = process.env.PORT;
+console.log(PORT);
+
+app.get('/status', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
+console.log(process.env.NODE_ENV);
+
+//make ready for production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (_, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    });
+  }
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}` );
+}   );
