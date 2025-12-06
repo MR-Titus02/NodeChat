43b7fad7e13d7f37a1a124e73ecdd9ed01029231
MR-Titus02 <titusroxsan@gmail.com>
2025-12-06 19:50:16 +0530

    rate limit implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { login, signup, logout, updateProfile } from '../controllers/auth.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
+import  arcjetProtection  from '../middleware/arcjet.middleware.js';
  
 const router = express.Router();
  
@@ -14,6 +15,10 @@ router.put('/update-profile', protectRoute, updateProfile );
  
 router.get('/check', protectRoute, (req, res) => {
     res.status(200).json({ message: "Authorized", user: req.user });
+});
+
+router.get('/arcjet-test', arcjetProtection, (req, res) => {
+    res.status(200).json({ message: "Request passed Arcjet protection" });
 });
  
  
