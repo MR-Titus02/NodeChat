b27641ba3bab08c52c4b7f4345b2033a3fcb6a88
MR-Titus02 <titusroxsan@gmail.com>
2025-12-09 17:22:48 +0530

    login & logout frontend && toast messages

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore';
 
 function ChatPage() {
+
+  const { logout } = useAuthStore();
   return (
+    <>
     <div>ChatPage</div>
+    <button className='z-10' onClick={logout}>LogOut</button>
+    </>
   )
 }
 
