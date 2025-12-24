c139205bda169fd7455453b61a5dd7cdd824a3c5
MR-Titus02 <titusroxsan@gmail.com>
2025-12-24 15:36:33 +0530

    online

--- frontend/src/components/ChatList.jsx
+++ frontend/src/components/ChatList.jsx
@@ -2,10 +2,12 @@ import React, { useEffect } from 'react'
 import { useChatStore } from '../store/useChatStore'
 import UsersLoadingSkeleton from './UsersLoadingSkeleton';
 import NoChatsFound from './NoChatsFound';
+import { useAuthStore } from '../store/useAuthStore';
 
 function ChatList() {
   const { getMyChatPartners, chats, isUsersLoading, setSelectedUser } = useChatStore();
-
+  const { onlineUsers } = useAuthStore();
+  
   useEffect(() => {
     getMyChatPartners()
   }, [getMyChatPartners])
@@ -22,8 +24,7 @@ function ChatList() {
       onClick={() => setSelectedUser(chat)} 
        > 
          <div className='flex items-center gap-3'> 
-           TODO : FIX this online sttaus and make it work with socket 
-           <div className={`avatar online`}>
+           <div className={`avatar ${onlineUsers.includes(String(chat._id)) ? "online" : "offline" }`}>
             <div className="size-12 rounded-full">
               <img src={chat.profilePic || "/avatar.png" } alt={chat.fullName} />
             </div>
